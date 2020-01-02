39ad150b6d97bb9af08ece38536cac90d120cc88
Felix Hillebrand <97hilfel@gmail.com>
2020-01-02 18:36:44 +0100

    Setup gradle
    Setup gradle and add necessary main class.

---
+++ src/main/java/org/astraljaeger/noticeree/NoticeRee.java
@@ -0,0 +1,16 @@
+package org.astraljaeger.noticeree;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class NoticeRee extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        System.exit(0);
+    }
+}
