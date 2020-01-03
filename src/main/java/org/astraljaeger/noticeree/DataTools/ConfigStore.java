71f4e6a3722d2da661063338b5ee8a6667e388c5
Felix Hillebrand <97hilfel@gmail.com>
2020-01-04 00:23:52 +0100

    Implement ConfigStore V1
    Stores data as json in plain text on drive

--- src/main/java/org/astraljaeger/noticeree/DataTools/ConfigStore.java
+++ src/main/java/org/astraljaeger/noticeree/DataTools/ConfigStore.java
@@ -8,7 +8,9 @@ import com.google.gson.Gson;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.logging.Logger;
 import org.astraljaeger.noticeree.Configuration;
 import org.astraljaeger.noticeree.Utils;
 
@@ -22,60 +24,93 @@ public class ConfigStore {
         return instance;
     }
 
-    private String location = Configuration.CONFIG_LOCATION;
+    private Logger log;
+
+    private String file;
+    private String dir;
+
     private String fileName = "config.eson";
-    private Path configPath = Path.of(location + fileName);
-    private Config config;
-    private File configFile;
+    private ConfigItem config;
+    private Gson serializer;
 
     private ConfigStore(){
+        // log = Logger.getLogger(this.getClass().getName());
+        serializer = new Gson();
+        char sep = File.separatorChar;
+
+        String os = System.getProperty("os.name").toLowerCase();
+        String workingDir = "";
+        if(os.contains("win")){
+            workingDir = System.getenv("AppData");
+        }
+        else {
+            workingDir = System.getProperty("user.home");
+            if(os.contains("mac"))
+                workingDir += sep + "Library" + sep + "Application Support";
+        }
+
+        dir = workingDir + sep +
+            Configuration.APP_LOCATION + sep +
+            Configuration.CONFIG_LOCATION + sep;
+        file = dir + fileName;
+        if(!Files.exists(Paths.get(file))){
+            try {
+                Files.createDirectories(Paths.get(dir));
+                Files.createFile(Paths.get(file));
+
+                var empty = new ConfigItem("");
+                Files.write(
+                    Paths.get(file),
+                    encrypt(serializer.toJson(empty)).getBytes(),
+                    StandardOpenOption.CREATE
+                );
+            }catch (IOException e){
+                // TODO
+                System.exit(1);
+            }
+        }
 
         try {
-            if (!Files.exists(configPath))
-                Files.createFile(configPath);
-            else {
-                var content = decrypt(Files.readString(configPath));
-                var serializer = new Gson();
-                config = serializer.fromJson(content, Config.class);
-            }
+
+            var content = Files.readString(Paths.get(file));
+            config = serializer.fromJson(decrypt(content), ConfigItem.class);
         }catch (IOException e){
-            var dialog = Utils.createErrorDialog(
-                e,
-                "Error",
-                "Creating " + this.getClass().getName() + "caused an error!"
-            );
-            dialog.show();
+            // TODO: Add error display
+            System.exit(2);
         }
     }
 
-    public Config getConfig(){
+    public ConfigItem getConfigItem(){
         return config;
     }
 
+    public void setToken(String token){
+        config.token = token;
+        writeFile(encrypt(serializer.toJson(config)));
+    }
+
     private String decrypt(String cipher){
-
-
-
         return cipher;
     }
 
     private String encrypt(String plain){
-
-
-
         return plain;
     }
 
     private void writeFile(String content){
         try {
-            Files.write(configPath, encrypt(content).getBytes());
+            Files.write(
+                Paths.get(file),
+                encrypt(content).getBytes(),
+                StandardOpenOption.TRUNCATE_EXISTING
+            );
         }catch (IOException e){
             var dialog = Utils.createErrorDialog(
               e,
               "Error writing to file",
               "An error occured while trying to write to the config file"
             );
-            dialog.show();
+            dialog.showAndWait();
         }
     }
 }
