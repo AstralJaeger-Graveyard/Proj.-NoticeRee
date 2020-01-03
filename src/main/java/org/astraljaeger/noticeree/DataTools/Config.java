1cb6581ea87c59891c1371e930bce7aa3e6c508f
Felix Hillebrand <97hilfel@gmail.com>
2020-01-03 22:28:38 +0100

    Add un-encrypted version of ConfigStore

--- src/main/java/org/astraljaeger/noticeree/DataTools/Config.java
+++ src/main/java/org/astraljaeger/noticeree/DataTools/Config.java
@@ -1,5 +1,16 @@
+/*
+ * Copyright (c) 2020.
+ */
+
 package org.astraljaeger.noticeree.DataTools;
+
+import javafx.scene.control.ComboBox;
 
 public class Config {
 
+    public String token;
+
+    public Config(String token){
+        this.token = token;
+    }
 }
