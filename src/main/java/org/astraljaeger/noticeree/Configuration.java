39e040fbc66332555847b5b23ec7d7551dfab0b6
Felix Hillebrand <97hilfel@gmail.com>
2020-01-03 22:29:10 +0100

    Add configuration class

--- src/main/java/org/astraljaeger/noticeree/Configuration.java
+++ src/main/java/org/astraljaeger/noticeree/Configuration.java
@@ -1,5 +1,14 @@
+/*
+ * Copyright (c) 2020.
+ */
+
 package org.astraljaeger.noticeree;
+
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
 
 public class Configuration {
 
+    public static String SEPARATOR = FileSystems.getDefault().getSeparator();
+    public static String CONFIG_LOCATION =  SEPARATOR + "config" + SEPARATOR;
 }
