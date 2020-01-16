87755411a596266f81abe0208ce571328a3d96bf
Felix Hillebrand <97hilfel@gmail.com>
2020-01-16 20:09:42 +0100

    Prepare for Login
    Took 38 minutes

--- src/main/java/org/astraljaeger/noticeree/Controllers/MainController.java
+++ src/main/java/org/astraljaeger/noticeree/Controllers/MainController.java
@@ -1,11 +1,12 @@
 package org.astraljaeger.noticeree.Controllers;
 
+import com.github.twitch4j.TwitchClient;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TableView;
 import javafx.stage.Stage;
+import org.astraljaeger.noticeree.DataTools.DataStore;
 
 public class MainController {
 
@@ -42,6 +43,8 @@ public class MainController {
     @FXML
     public Button applyFilterNoticeMeBtn;
 
+    DataStore store;
+    TwitchClient client;
 
     public MainController(){
 
@@ -50,12 +53,14 @@ public class MainController {
     @FXML
     public void initialize(){
 
+        client = doLogin();
+
         mainTp.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-            System.out.println("Changed tab from \'" +
+            System.out.println("Changed tab from '" +
                     mainTp.getTabs().indexOf(oldValue) +
-                    "\' to \'" +
+                    "' to '" +
                     mainTp.getTabs().indexOf(newValue) +
-                    "\'");
+                    "'");
 
 
 
@@ -64,6 +69,12 @@ public class MainController {
 
         
 
+    }
+
+    private TwitchClient doLogin() {
+
+
+        return null;
     }
 
     public void setPrimaryStage(Stage primaryStage){
--- src/main/java/org/astraljaeger/noticeree/Utils.java
+++ src/main/java/org/astraljaeger/noticeree/Utils.java
@@ -8,12 +8,21 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
+import javafx.util.Pair;
 
 public class Utils {
+
+    public static Dialog<Pair<String, Boolean>> createLoginDialog(String errorMessage){
+
+        Dialog<Pair<String, Boolean>> dialog = new Dialog<>();
+
+        return dialog;
+    }
 
     public static Alert createErrorDialog(Exception e, String title, String header){
         Alert errorAlert = new Alert(AlertType.ERROR);
