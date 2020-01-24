64d62d8eb602f5b93a270f846a8c4cb198dc43af
Felix Hillebrand <97hilfel@gmail.com>
2020-01-24 11:13:22 +0100

    Start with empty DataStore
    Took 2 minutes

--- src/main/java/org/astraljaeger/noticeree/DataTools/DataStore.java
+++ src/main/java/org/astraljaeger/noticeree/DataTools/DataStore.java
@@ -4,17 +4,10 @@
 
 package org.astraljaeger.noticeree.DataTools;
 
-import org.astraljaeger.noticeree.Configuration;
-import org.dizitart.no2.Nitrite;
-import org.dizitart.no2.NitriteCollection;
-
 public class DataStore {
 
     private static DataStore instance;
     private static final String FILE_NAME = "data.nosql";
-    private static final String WELCOME_MSG_COLLECTION = "welcomeMsgCollection";
-    private static final String NOTICE_ME_COLLECTION = "noticeMeCollection";
-
 
     public static DataStore getInstance(){
         if(instance == null)
@@ -22,23 +15,8 @@ public class DataStore {
         return instance;
     }
 
-    private final Nitrite db;
-    private final NitriteCollection welcomeMsgColletion;
-    private final NitriteCollection noticeMeCollection;
-
     private DataStore(){
 
-        db = Nitrite.builder()
-                .filePath(Configuration.getAppDataDirectory() + FILE_NAME)
-                .openOrCreate();
-
-        welcomeMsgColletion = db.getCollection(WELCOME_MSG_COLLECTION);
-        noticeMeCollection = db.getCollection(NOTICE_ME_COLLECTION);
     }
-
-    public void addWelcomeMsg(){
-
-    }
-
 
 }
